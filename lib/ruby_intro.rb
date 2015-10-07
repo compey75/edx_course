41cb44e82c79facd626aef0c9cd3a8c01c9d3759
compey <lramirez57@hotmail.com>
2015-10-07 07:49:06 -0500

    parcial_ejercicio1
    Agrego solo ejercicio1 parte 1

---
+++ lib/ruby_intro.rb
@@ -0,0 +1,43 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum arr
+  if arr.length == 0  then
+     return 0
+  else
+    	suma = 0.0
+        for x in 0..arr.length 
+            suma += arr[x].to_f
+        end 
+  end
+return suma 
+end
+
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
+
+# Part 2
+
+def hello(name)
+  # YOUR CODE HERE
+end
+
+def starts_with_consonant? s
+  # YOUR CODE HERE
+end
+
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# YOUR CODE HERE
+end
